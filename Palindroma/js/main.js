81e2dc63b1d8f8c1ef5d877e87efe6751898c12d
Luca <lucaciprani1@gmail.com>
2021-07-23 14:53:49 +0200

    Palindroma

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,2 +1,46 @@
-// palindroma
+// Palindroma
+
+// Chiedere all’utente di inserire una parola
+
+// var utenteParola = prompt("Inserisci una parola");
+// console.log(utenteParola);
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// metodo normale 
+
+// var parolaDivisa = utenteParola.split('');
+// console.log(parolaDivisa);
+
+// var parolaDivisa = parolaDivisa.reverse();
+// console.log(parolaDivisa);
+
+// var parolaInvertita = parolaDivisa.join('');
+// console.log(parolaInvertita);
+
+// // condizione
+// if(utenteParola == parolaInvertita){
+//     console.log('la parola è palindroma');
+//   } else {
+//     console.log('la parola non è palindroma');
+//   }
+  
+
+//   con la funzione;
+
+// var utenteParola = prompt("Inserisci una parola");
+// console.log(utenteParola);
+
+// var parolaInversa = invertiParola(utenteParola);
+
+// if(parolaInversa == parolaUtente) {
+//     console.log("la parola è palindroma");
+// } else {
+//     console.log("la parola non è palindroma");
+// }
+
+// function invertiParola(str) {
+//     var strInversa  = str.split(" ").reverse().join("");
+//     return strInversa;
+// }
 
